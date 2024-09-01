abd1e98affa6eee5b1f1c41431bdae5b4f2ac0b2
Sergei Oskolkov <SergeyOsk2017@gmail.com>
2024-09-01 21:45:39 +0200

    Implement ProjectsListAPIView with GET and POST methods, add serializers

---
+++ apps/projects/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from apps.projects.views.project_views import ProjectsListAPIView
+
+urlpatterns = [
+    path('', ProjectsListAPIView.as_view(), name='project-list-create'),
+]
--- apps/routers.py
+++ apps/routers.py
@@ -2,4 +2,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('tasks/', include('apps.tasks.urls')),
+    path('projects/', include('apps.projects.urls')),
 ]
